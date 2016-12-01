869cbb30e616a14fab15611efe5c2501d3274928
thib-s <thibaut.boissin@gmail.com>
2016-12-01 12:54:37 +0100

    Correction de tous les beugs, le fichier compile

--- linda/linda/shm/CentralizedLinda.java
+++ linda/linda/shm/CentralizedLinda.java
@@ -4,60 +4,70 @@ import linda.Callback;
 import linda.Linda;
 import linda.Tuple;
 import java.util.*;
+import java.util.concurrent.ThreadPoolExecutor;
 
 /** Shared memory implementation of Linda. */
 public class CentralizedLinda implements Linda {
 	
-	List<Tuple> tupleSpace;
+	private List<Tuple> tupleSpace;
 
     public CentralizedLinda() {
 		tupleSpace = new ArrayList<Tuple>();
     }
 
+    @Override
 	public void write(Tuple t) {
 		tupleSpace.add(t);
 	}
 
 	// First version : it is assumed that the template matches a tuple in the tupleSpace
 	// Need to block if there is no matching tuple
+	@Override
 	public Tuple take(Tuple template) {
+        Tuple t = null;
 		int i = findNext(0, template);
 		if (i != -1) {
-			Tuple t = tupleSpace.remove(i);
+			t = tupleSpace.remove(i);
 		} 
 		return t;
 	}
 
+	@Override
 	public Tuple read(Tuple template) {
-		Tuple result;
+		Tuple result = null;
 		int index = findNext(0, template);
 		if (index == -1){
 			//sleep
 		} else {
 			result = tupleSpace.get(index);
 		}
+		return result;
 	}
 
+	@Override
 	public Tuple tryTake(Tuple template) {
 		int i = findNext(0, template);
+        Tuple t;
 		if (i != -1) {
-			Tuple t = tupleSpace.remove(i);
+			t = tupleSpace.remove(i);
 		} else {
-			Tuple t = null;
+			t = null;
 		}
 		return t;
 	}
 
+	@Override
 	public Tuple tryRead(Tuple template) {
-
+        return new Tuple();
 	}
 
+	@Override
 	public List<Tuple> takeAll(Tuple template) {
 		List<Tuple> listMatches = new ArrayList<Tuple>();
 		
 		int i = 0;
 		while (i < tupleSpace.size()) {
-			i = findNext(i); // i is the index of template's next occurence 
+			i = findNext(i, template); // i is the index of template's next occurence
 			// If there is no more matching tuples in the tuplespace,
 			// quit the loop
 			if (i == -1) {
@@ -65,7 +75,7 @@ public class CentralizedLinda implements Linda {
 			}
 			// Else, pop the matching tuple and add it to the list
 			else {
-				listMatches.add(tupleSpace.remove(index));
+				listMatches.add(tupleSpace.remove(i));
 			}
 			i++;
 		}
@@ -73,10 +83,12 @@ public class CentralizedLinda implements Linda {
 		return listMatches;
 	}
 
+	@Override
 	public Collection<Tuple> readAll(Tuple template) {
-		
+		return new ArrayList<Tuple>();
 	}
 
+	@Override
 	public void eventRegister(eventMode mode, eventTiming timing, Tuple template, Callback callback) {	
 
 	}
@@ -92,7 +104,7 @@ public class CentralizedLinda implements Linda {
 		int result;
 		boolean notfound = true;
 		while (notfound && ( i < this.tupleSpace.size())){
-			if (Tuple.matches(tupleSpace(i), template)){
+			if (tupleSpace.get(i).matches(template)){
 				notfound = false;
 			}
 			i++;
@@ -106,7 +118,8 @@ public class CentralizedLinda implements Linda {
 		return result;
 	}
 
-	public void debug(String prefix) {
+	@Override
+    public void debug(String prefix) {
 
 	}
 
