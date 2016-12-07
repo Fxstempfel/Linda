552cf7329cccb89be5171d09ac9414292d5fa4de
François-Xavier Stempfel <fxstempfel@gmail.com>
2016-12-07 15:44:06 +0100

    version qui marche ?git status de linda

--- linda/shm/CentralizedLinda.java
+++ linda/shm/CentralizedLinda.java
@@ -11,30 +11,44 @@ import java.util.logging.Logger;
 public class CentralizedLinda implements Linda {
 
 	private List<Tuple> tupleSpace;
-	private Map<Tuple, Callback> pendingReads;
-	private Map<Tuple, Callback> pendingTakes;
+	// we use the queue interface to handle multiple callbacks over one key
+	private Map<Tuple, Queue<Callback>> pendingReads;
+	private Map<Tuple, Queue<Callback>> pendingTakes;
 
     public CentralizedLinda() {
 		tupleSpace = new ArrayList<Tuple>();
-		pendingTakes = new HashMap<Tuple, Callback>();
-		pendingReads = new HashMap<Tuple, Callback>();
+		pendingTakes = new HashMap<Tuple, Queue<Callback>>();
+		pendingReads = new HashMap<Tuple, Queue<Callback>>();
     }
 
 	public void write(Tuple t) {
 		boolean matchingTake = false;
 		synchronized (this) {
+			// Call the callbacks of the pending reads on the matching template
 			for (Tuple template : pendingReads.keySet()) {
 				if (t.matches(template)) {
-					pendingReads.remove(template).call(t.deepclone());
+					for (Callback c : pendingReads.remove(template)) {
+						c.call(t.deepclone());
+					}
 				}
 			}
+			// Call the callback of the first occurence of the pending takes on
+			// the key matching the template
 			for (Tuple template : pendingTakes.keySet()) {
 				if (t.matches(template)) {
-					pendingTakes.remove(template).call(t.deepclone());
+					Queue<Callback> queue = pendingTakes.get(template); 
+					// Remove the head of the queue
+					queue.remove().call(t.deepclone());
+					if (queue.isEmpty()) {
+						// Remove the key if there is no more pending takes
+						pendingTakes.remove(template);
+					}
 					matchingTake = true;
 					break;
 				}
 			}
+			// If there is no pending take over the tuple t, add it to the
+			// tupleSpace
 			if (!matchingTake) {
 				tupleSpace.add(t.deepclone());
 			}
@@ -46,12 +60,12 @@ public class CentralizedLinda implements Linda {
 		eventRegister(Linda.eventMode.TAKE, Linda.eventTiming.IMMEDIATE,
 				template, callback);
 		synchronized (callback) {
-			while (callback.result == null) {
+			//while (callback.result == null) {
 				try {
 					callback.wait();
 				} catch(Exception e) {
 				}
-			}
+			//}
 			return callback.result;
 		}
 	}
@@ -61,12 +75,12 @@ public class CentralizedLinda implements Linda {
 		eventRegister(Linda.eventMode.READ, Linda.eventTiming.IMMEDIATE,
 				template, callback);
 		synchronized (callback) {
-			while (callback.result == null) {
+			//while (callback.result == null) {
 				try {
 					callback.wait();
 				} catch(Exception e) {
 				}
-			}
+			//}
 			return callback.result;
 		}
 	}
@@ -125,6 +139,7 @@ public class CentralizedLinda implements Linda {
 
 	public void eventRegister(eventMode mode, eventTiming timing, Tuple template, Callback callback) {	
 		Tuple t=null;
+		boolean tupleFound = false;
 		synchronized (this) {
 			if (timing == eventTiming.IMMEDIATE) {
 				if (mode == eventMode.TAKE) {
@@ -134,28 +149,46 @@ public class CentralizedLinda implements Linda {
 					t = tryRead(template);
 				}
 				if (t == null) {
+					// If the tuple was not found in the tupleSpace, add it to
+					// the pending operations
 					if (mode == eventMode.TAKE) {
-						pendingTakes.put(template, callback);
+						addCallbackToQueue(pendingTakes, template, callback);
 					} else {
-						pendingReads.put(template, callback);
+						addCallbackToQueue(pendingReads, template, callback);
 					}
 				} else {
-					callback.call(t); // Retirer du synchronized ?
+					tupleFound = true;
 				}
 			} else {
 				// timing = FUTURE
+				// Add the callback to the pending operations
 				if (mode == eventMode.TAKE) {
-					pendingTakes.put(template, callback);
+					addCallbackToQueue(pendingTakes, template, callback);
 				} else {
 					// mode = READ
-					pendingReads.put(template, callback);
+					addCallbackToQueue(pendingReads, template, callback);
 				}
 
 			}
 		}
+		// If the tuple was found in the tupleSpace, run the callback
+		if (tupleFound) {
+			callback.call(t); 
+		}
 	}
 
-
+	private void addCallbackToQueue(Map<Tuple, Queue<Callback>> map, Tuple template, Callback callback) {
+		// If the key already exists, add the callback to the
+		// corresponding queue
+		if (map.containsKey(template)) {
+			map.get(template).add(callback);
+		} else {
+			// Else, create a new queue
+			Queue<Callback> newQueue = new LinkedList<Callback>();
+			newQueue.add(callback);
+			map.put(template, newQueue);
+		}
+	}
 
     public void debug(String prefix) {
 		// Print the tuples in the tupleSpace
